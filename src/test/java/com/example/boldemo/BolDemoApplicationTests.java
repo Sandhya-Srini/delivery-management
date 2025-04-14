5ba066cb09ed59e9ae7feeba20008c5b895fcfb8
SandhyaSrinivasan <ssrinivasan@blinkcharging.com>
2025-04-14 10:54:56 +0200

    initial commit

---
+++ src/test/java/com/example/boldemo/BolDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
